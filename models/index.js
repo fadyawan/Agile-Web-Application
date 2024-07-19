6c8c28e37399760442f792cdeb646f88ad2d8d63
BlakeMedlock1 <m044803m@student.staffs.ac.uk>
2024-07-19 10:36:10 +0100

    added index.js

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+    host: config.HOST,
+    dialect: config.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.skillAssignment = require("./skillAssignment")(sequelize, Sequelize);
+db.skill = require("./skill")(sequelize, Sequelize);
+
+// Define relationships if any
+// e.g., db.skillAssignment.belongsTo(db.skill);
+
+module.exports = db;
