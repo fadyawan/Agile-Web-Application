0b9fdfb9fb398c89fad1672af6e906d5b89c214c
lewis redhead <lewis.redhead@capula.com>
2024-07-21 19:03:41 +0100

    api tests updates

--- routes/skillAssignment.js
+++ routes/skillAssignment.js
@@ -3,13 +3,10 @@ const controller = require('../controllers/skillAssignment');
 const express = require('express');
 const router = express.Router();
 
-// Some of these endpoints don't run properly, mainly delete, there were some endpoints that had the same call conditions with differing functions being called from the endpoint.
-
 router.get('/', controller.getAll);
 router.get('/id/:id', controller.getById);
-router.get('/skill/:skill', controller.getBySkills);
-router.get('/name/:firstname/:surname', controller.getByStaff);
-router.get('/staffDetails', controller.getAllStaffDetails);
+router.get('/skill/:skill_id', controller.getBySkills);
+router.get('/staff/:staff_id', controller.getByStaff);
 
 router.post('/', controller.create);
 router.delete('/', controller.deleting);
